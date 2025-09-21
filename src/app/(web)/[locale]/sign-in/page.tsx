c3c2b2364e4e0d91bd947f3c4a6cd122f640367b
Samvel <loidnoir@gmail.com>
2025-09-21 19:50:14 +0400

    feat(auth): sign-up/in pages

---
+++ src/app/(web)/[locale]/sign-in/page.tsx
@@ -0,0 +1,5 @@
+import { LoginComponent } from '@/modules/login'
+
+export default function SignInPage() {
+  return <LoginComponent />
+}
--- src/modules/sign-up/sign-up.component.tsx
+++ src/modules/sign-up/sign-up.component.tsx
@@ -11,108 +11,109 @@ import {
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
 } from '@/shared/ui/form'
 import { Input } from '@/shared/ui/input'
+import { CircleCheck, CircleX } from 'lucide-react'
 import { useSignUpService } from './sign-up.service'
 
 const SignUpComponent = () => {
   const service = useSignUpService()
 
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>{service.t('title')}</CardTitle>
-        <CardDescription>{service.t('description')}</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <Form {...service.form}>
-          <form
-            onSubmit={service.form.handleSubmit(service.onSubmit)}
-            className='space-y-8'
-          >
-            {service.error && (
-              <div className='text-sm text-red-600 bg-red-50 p-3 rounded-md'>
-                {service.error}
+    <div className='h-full flex items-center justify-center'>
+      <Card className='w-[450px]'>
+        <CardHeader>
+          <CardTitle>{service.t('title')}</CardTitle>
+          <CardDescription>{service.t('description')}</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...service.form}>
+            <form
+              onSubmit={service.form.handleSubmit(service.onSubmit)}
+              className='space-y-6'
+            >
+              <div className='space-y-4'>
+                <FormField
+                  control={service.form.control}
+                  name='username'
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>
+                        {service.t('fields.username.label')}
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder={service.t('fields.username.placeholder')}
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={service.form.control}
+                  name='email'
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>{service.t('fields.email.label')}</FormLabel>
+                      <FormControl>
+                        <Input
+                          type='email'
+                          placeholder={service.t('fields.email.placeholder')}
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={service.form.control}
+                  name='password'
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>
+                        {service.t('fields.password.label')}
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          type='password'
+                          placeholder={service.t('fields.password.placeholder')}
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
               </div>
-            )}
-            {service.success && (
-              <div className='text-sm text-green-600 bg-green-50 p-3 rounded-md'>
-                {service.t('messages.success')}
-              </div>
-            )}
-            <FormField
-              control={service.form.control}
-              name='username'
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>{service.t('fields.username.label')}</FormLabel>
-                  <FormControl>
-                    <Input
-                      placeholder={service.t('fields.username.placeholder')}
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormDescription>
-                    {service.t('fields.username.description')}
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
+              {service.error && (
+                <div className='text-sm text-destructive flex gap-1 items-top'>
+                  <CircleX size={20} />
+                  {service.error}
+                </div>
               )}
-            />
-            <FormField
-              control={service.form.control}
-              name='email'
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>{service.t('fields.email.label')}</FormLabel>
-                  <FormControl>
-                    <Input
-                      type='email'
-                      placeholder={service.t('fields.email.placeholder')}
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormDescription>
-                    {service.t('fields.email.description')}
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
+              {service.success && (
+                <div className='text-sm text-green-400 flex gap-1 items-top'>
+                  <CircleCheck size={20} />
+                  {service.t('messages.success')}
+                </div>
               )}
-            />
-            <FormField
-              control={service.form.control}
-              name='password'
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>{service.t('fields.password.label')}</FormLabel>
-                  <FormControl>
-                    <Input
-                      type='password'
-                      placeholder={service.t('fields.password.placeholder')}
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormDescription>
-                    {service.t('fields.password.description')}
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <Button type='submit' disabled={service.isLoading}>
-              {service.isLoading
-                ? service.t('actions.submitting')
-                : service.t('actions.submit')}
-            </Button>
-          </form>
-        </Form>
-      </CardContent>
-    </Card>
+              <Button type='submit' disabled={service.isLoading}>
+                {service.isLoading
+                  ? service.t('actions.submitting')
+                  : service.t('actions.submit')}
+              </Button>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </div>
   )
 }
 
