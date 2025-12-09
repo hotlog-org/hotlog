2f24bf32cf9787a5adc43fdcfee8a6e55c6171f0
Samvel <loidnoir@gmail.com>
2025-12-09 22:12:31 +0400

    fix: small ui fixes

--- src/modules/overview/fields/tables/roles/table/roles-table.service.tsx
+++ src/modules/overview/fields/tables/roles/table/roles-table.service.tsx
@@ -134,7 +134,7 @@ export const useRolesTableService = ({
                 <div className='flex items-center gap-2'>
                   <Button
                     variant='outline'
-                    size='xs'
+                    size='sm'
                     className='border-dashed text-xs'
                     onClick={() => toggleExpanded(row.original.id)}
                   >
@@ -147,7 +147,7 @@ export const useRolesTableService = ({
                   {expandedRoleIds[row.original.id] && (
                     <Button
                       variant='ghost'
-                      size='xs'
+                      size='sm'
                       className='text-xs'
                       onClick={() => toggleExpanded(row.original.id)}
                     >
